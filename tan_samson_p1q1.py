60d06c22c5da7e7bbf2df1db8c8cd1ac126e4f6e
Samson <samsontan795@gmail.com>
2021-09-23 11:08:33 -0700

    added a bunch of things

---
+++ tan_samson_p1q1.py
@@ -0,0 +1,12 @@
+
+import time
+score = 1
+print ("Hi, I'm Fricking Quiz Bot!")
+print ("I'll bother you with multiple annoying and easy questions while acting condescendingly if you get them wrong!")
+print ("Now for the first question:\n")
+time.sleep(2)
+print ("What is my name?")
+answer1 = input().lower().strip(",.!?")
+
+if answer == ("fricking quiz bot"):
+    score + 1
