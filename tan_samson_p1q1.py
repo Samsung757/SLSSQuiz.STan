a55055c0b6386abdf8eab8d5930d3328aa6c9e45
Samson <samsontan795@gmail.com>
2021-09-23 11:18:47 -0700

    added a bunch of things

--- tan_samson_p1q1.py
+++ tan_samson_p1q1.py
@@ -1,6 +1,25 @@
+# Quiz Creation Activity
 
+# Quiz has 5 questions the user will answer
+# It will keep track of score and give a final result
 import time
+import random
+
+random_congrats = ["Wow! You got that incredibly difficult question right! I'm so proud of you!",
+                   "Oh! I can't believe you got that right! That was such a hard question!",
+                   "Hmm... It seems that you're not as incompetent as I thought! I'll have to make harder questions next time!",
+                   "So impressive!",
+                   "You little tryhard.",
+                   "Butbutbut... How could you have gotten such a difficult question right??"]
+
+random_failures = ["Ha! I knew you would fail that one!",
+                   "You're so incompetent you couldn't even solve this question? You don't even deserve to do my quiz.",
+                   "Your mother would be so disappointed in you right now",
+                   "Oh too bad, I'm sure you'll get the next one right. Actually, no I'm not, you little failure.",
+                   ]
+
 score = 1
+
 print ("Hi, I'm Fricking Quiz Bot!")
 print ("I'll bother you with multiple annoying and easy questions while acting condescendingly if you get them wrong!")
 print ("Now for the first question:\n")
@@ -8,5 +27,5 @@ time.sleep(2)
 print ("What is my name?")
 answer1 = input().lower().strip(",.!?")
 
-if answer == ("fricking quiz bot"):
-    score + 1
+if answer1 == ("fricking quiz bot"):
+    score = score + 1
